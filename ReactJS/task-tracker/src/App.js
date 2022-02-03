5490652fb610744647f6f3426a5049711e2f05ed
Oliver Thorn <thornow11@gmail.com>
2022-02-03 15:21:13 +0000

    Routing

--- ReactJS/task-tracker/src/App.js
+++ ReactJS/task-tracker/src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
 const App = () => {
 
@@ -85,15 +88,27 @@ const App = () => {
   }
 
   return (
-    <div className='container'>
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask ? < AddTask onAdd={addTask} /> : ''}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        'No tasks to show'
-      )}
-    </div>
+    <Router>
+      <div className='container'>
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+
+        <Routes>
+          <Route path='/' exact element={
+            <>
+              {showAddTask && < AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+              ) : (
+                'In terminal, npm run server to emulate db to interact with db.json'
+              )}
+            </>
+          }></Route>
+          <Route path='/about' element={<About />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
