aabeeb3978ed6bdb8e3a7ca04654f05f4cfb0a9b
Oliver Thorn <thornow11@gmail.com>
2022-02-03 12:17:58 +0000

    UI Complete

--- ReactJS/task-tracker/src/components/Header.js
+++ ReactJS/task-tracker/src/components/Header.js
@@ -1,22 +1,22 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-
-    const onClick = () => {
-        console.log('Click')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
+            <Button
+                color={showAdd ? 'red' : 'green'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd}
+            />
         </header>
     )
 };
 
 Header.defaultProps = {
-    title: 'Task Tracker v1'
+    title: 'Task Tracker'
 }
 
 Header.propTypes = {
