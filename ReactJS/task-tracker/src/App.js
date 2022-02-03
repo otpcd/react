1112da06c60faccef60a2ecf8af083a3b6f60bac
Oliver Thorn <thornow11@gmail.com>
2022-02-03 10:02:03 +0000

    States

--- ReactJS/task-tracker/src/App.js
+++ ReactJS/task-tracker/src/App.js
@@ -1,10 +1,39 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      date: 'Jan 5',
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: 'Football',
+      date: 'Jan 5',
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'Homework',
+      date: 'Jan 5',
+      reminder: false,
+    },
+  ])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (
     <div className='container'>
       <Header />
+      {<Tasks tasks={tasks} onDelete={deleteTask} />}
     </div>
   );
 }
