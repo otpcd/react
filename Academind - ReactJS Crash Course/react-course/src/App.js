2a6221034c4eb77797b3373865b6f209cf494b6b
Oliver Thorn <thornow11@gmail.com>
2022-02-22 13:46:44 +0000

    Basics

---
+++ Academind - ReactJS Crash Course/react-course/src/App.js
@@ -0,0 +1,14 @@
+import Task from './components/Task'
+
+function App() {
+  return (
+    <div>
+      <h1>My Tasks</h1>
+      <Task text='Learn React' />
+      <Task text='Master React' />
+      <Task text='Finish updating project' />
+    </div>
+  );
+}
+
+export default App;
